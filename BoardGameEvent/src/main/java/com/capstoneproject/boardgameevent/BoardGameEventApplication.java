ba87f89fc1724eb639ef84c9b79cff18251e5bb7
Ramil Islamov <ramil.islamov.88@mail.ru>
2024-03-05 18:39:49 +0400

    init commit

---
+++ BoardGameEvent/src/main/java/com/capstoneproject/boardgameevent/BoardGameEventApplication.java
@@ -0,0 +1,13 @@
+package com.capstoneproject.boardgameevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGameEventApplication.class, args);
+	}
+
+}
---
+++ BoardGameEvent/src/test/java/com/capstoneproject/boardgameevent/BoardGameEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstoneproject.boardgameevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
